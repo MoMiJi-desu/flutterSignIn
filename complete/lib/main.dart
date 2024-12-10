5f191f58a098a3b08ff9f4494b93ead7b5209da4
MoMiJi-desu <martin659874123@gmail.com>
2024-12-10 19:52:35 +0800

    Update main.dart
    add comment

--- complete/lib/main.dart
+++ complete/lib/main.dart
@@ -7,11 +7,11 @@ import 'firebase_options.dart';
 // TODO(codelab user): Get API key
 const clientId = 'YOUR_CLIENT_ID';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
+void main() async { //定義主程式的進行  首先進行Flutter的初始化 再進行 Firebase的初始化 再執行我們的APP 確保順序執行正確才不會產生BUG
+  WidgetsFlutterBinding.ensureInitialized();//Flutter的初始化
+  await Firebase.initializeApp(// Firebase的初始化
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
+  //執行我們的APP
   runApp(const MyApp());
 }
